50884ecf58f90eb8f5d9165d5851d4de10f870bc
Tomas337 <Tomasdavid337@seznam.cz>
2025-03-02 15:38:24 +0100

    fix: added correct database file path

--- BlazorGame/Data/AppDbContext.cs
+++ BlazorGame/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite("Data Source=/home/tomas337/BlazorGameDB/blazorgamedb.sqlite");
+        => options.UseSqlite("Data Source=/home/tomas337/RiderProjects/BlazorGame/BlazorGameDB/blazorgamedb.sqlite");
 }
 
 public class User
