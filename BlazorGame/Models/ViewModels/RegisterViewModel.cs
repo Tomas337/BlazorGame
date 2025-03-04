41767603eb516d24d9935ecd9f47cf8394814619
Tomas337 <Tomasdavid337@seznam.cz>
2025-03-04 17:21:56 +0100

    feat: basic UI for registration

--- BlazorGame/Models/ViewModels/RegisterViewModel.cs
+++ BlazorGame/Models/ViewModels/RegisterViewModel.cs
@@ -2,8 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BlazorGame.Models.ViewModels;
 
-public class LoginViewModel
+public class RegisterViewModel
 {
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide an e-mail")]
+    public string? Email { get; set; }
+
     [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide a user name")]
     public string? Username { get; set; }
 
